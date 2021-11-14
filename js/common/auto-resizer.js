10a937de177aeea1bc4f7a3f633126b6e9d42c56
cruzhansm <cruzhansm@gmail.com>
2021-11-14 18:36:49 +0800

    50% done for help-center. Minor fixes for all files.

--- js/common/auto-resizer.js
+++ js/common/auto-resizer.js
@@ -3,9 +3,8 @@ window.onload = () => {
   const right = document.querySelector('.col.right');
   const lastElem = right.lastElementChild;
 
-  if(right.offsetHeight == 1080 && lastElem.offsetHeight != 1080) {
-    const offset = right.offsetHeight - lastElem.offsetHeight;
-    right.style.minHeight = `${(1080 - offset).toString()}px`;
-    console.log(right.offsetHeight); 
+  if(right.offsetHeight > 1080) {
+    const height = (right.offsetHeight - lastElem.offsetHeight);
+    right.style.height = `${height.toString()}px`;
   }
 }
