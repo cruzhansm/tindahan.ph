27a8b79a7545a930f70f8f8fe6d51867b23af636
cruzhansm <cruzhansm@gmail.com>
2021-11-19 00:01:17 +0800

    Product page template done. Awaiting JS and backend integration.

--- js/common/auto-resizer.js
+++ js/common/auto-resizer.js
@@ -1,10 +1,14 @@
-window.onload = () => {
+// window.onload = () => {
   
-  const right = document.querySelector('.col.right');
-  const lastElem = right.lastElementChild;
+//   const right = document.querySelector('.col.right');
+//   const lastElem = right.lastElementChild;
 
-  if(right.offsetHeight > 1080) {
-    const height = (right.offsetHeight - lastElem.offsetHeight);
-    right.style.height = `${height.toString()}px`;
-  }
-}
+//   console.log(right.offsetHeight);
+//     console.log(lastElem.offsetHeight);
+
+//   if(right.offsetHeight > 1080) {
+    
+//     const height = (right.offsetHeight - lastElem.offsetHeight);
+//     right.style.height = `${height.toString()}px`;
+//   }
+// }
