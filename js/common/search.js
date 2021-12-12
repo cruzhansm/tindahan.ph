1f5a504cb17377db524ffb45d42f03f8f4de1ddf
Hans Maco Cruz <cruzhansm@gmail.com>
2021-12-13 03:48:19 +0800

    Hotfixes for search (#38)
    * Hotfix for search.html
    
    * Hotfix for search.js

--- js/common/search.js
+++ js/common/search.js
@@ -1,32 +1,30 @@
 window.onload = () => {
-  if(window.location.href.includes('search.html')) {
+  if (window.location.href.includes('search.html')) {
     const search = new URLSearchParams(window.location.search).get('q');
     const searchTitle = document.querySelector('#sq');
-    
+
     // REFACTOR
     // Once backend is done, revert to false and do a search query
     // If search string is found, assign true and update query title
     const validSearch = search != null ? true : false;
 
-    let searchFound = true; 
+    // Should be true or false depending if there are products found
+    let searchFound = true;
 
     // REFACTOR
     // Change to another appropriate function that appends select products only
-    if(validSearch && searchFound) { 
-      fetchProductsByBatch(); 
+    if (validSearch && searchFound) {
       searchTitle.innerText = `"${search}"`;
-    }
-    else {
-      searchTitle.parentElement.innerHTML = "Results"
+    } else {
+      searchTitle.parentElement.innerHTML = 'Results';
     }
   }
-} 
+};
 
 function search(event) {
-
   const query = event.target.firstElementChild.value;
-  let redirect = '../../src/common/search.html';
-  
+  let redirect = '/tindahan.ph/src/common/search.html';
+
   event.preventDefault();
 
   window.location.href = query.length > 0 ? `${redirect}?q=${query}` : redirect;
