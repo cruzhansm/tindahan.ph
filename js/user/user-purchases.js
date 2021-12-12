1e1727fa3deeb2e583aff27b805ee619aa232a63
cruzhansm <cruzhansm@gmail.com>
2021-12-12 23:05:55 +0800

    Added pagination

--- js/user/user-purchases.js
+++ js/user/user-purchases.js
@@ -3,6 +3,7 @@ import { cancelSpecifiedOrder } from './db-methods/update.js';
 import { noSubmit } from '../common/input/form.js';
 import { attachCharCountListener } from '../common/input/input.js';
 import { createProductReview } from './db-methods/insert.js';
+import { Pagination } from '../common/pagination.js';
 
 var PURCHASES = new Array();
 
@@ -30,77 +31,92 @@ window.onload = async () => {
 };
 
 function appendAllPurchases(ordered) {
-  const container = document.querySelector('.container-orders-list');
+  const pagination = new Pagination(
+    'paginationContainer',
+    ordered.length / 3,
+    ordered.length > 0 ? true : false
+  );
 
-  ordered.forEach((order) => {
-    let ordersGroup = document.createElement('div');
-    ordersGroup.classList.add('orders-group');
-    ordersGroup.setAttribute('id', `og${order.orderID}`);
+  let limit = 3;
+  let idx = 0;
 
-    ordersGroup.innerHTML = `
-    <div class="order-header">
-      <div class="order-header-details">
-        <div class="fs-18">Order#${order.orderID}</div>
-      </div>
-      <div class="order-header-price">P${order.toPay}</div>
-    </div>
-    `;
+  for (let currentPage = 0; currentPage < pagination.pageTotal; currentPage++) {
+    let target = document.querySelector(`#page${currentPage + 1}`);
 
-    let productGroup = document.createElement('div');
-    productGroup.classList.add('order-product-group');
+    for (let num = 1; num <= limit && idx < ordered.length; num++) {
+      let order = ordered[idx++];
+      let ordersGroup = document.createElement('div');
+      ordersGroup.classList.add('orders-group');
+      ordersGroup.setAttribute('id', `og${order.orderID}`);
 
-    order.orders.orders.forEach((product) => {
-      productGroup.innerHTML += `
-      <div class="order-product purchases">
-        <div class="order-product-details">
-        <i class="fa-solid fa-comment-dots ${
-          order.orders.orderStatus != 'delivered' ? 'tph-disabled' : ''
-        }" onclick="attemptProductReview(productReview, ${
-        product.product_id
-      }, ${product.order_product_id})" style="${
-        product.review_id != null
-          ? 'color: var(--tph-highlight); pointer-events: none;'
-          : ''
-      }"></i>
-          <img src="${product.product_img}" class="order-product-img" />
-          <div class="order-product-info">
-            <div class="order-product-name">${product.product_name}</div>
-            <div class="order-product-variation">${product.variation}</div>
-            <div class="order-product-quantity">x${product.order_quantity}</div>
-          </div>
+      ordersGroup.innerHTML = `
+      <div class="order-header">
+        <div class="order-header-details">
+          <div class="fs-18">Order#${order.orderID}</div>
         </div>
-        <div>P${product.order_price}</div>
+        <div class="order-header-price">P${order.toPay}</div>
       </div>
       `;
-    });
 
-    let orderStatus = document.createElement('div');
-    orderStatus.classList.add('order-status-list');
+      let productGroup = document.createElement('div');
+      productGroup.classList.add('order-product-group');
 
-    orderStatus.innerHTML = `
-      <div class="order-product-status deliver">
+      order.orders.orders.forEach((product) => {
+        productGroup.innerHTML += `
+        <div class="order-product purchases">
+          <div class="order-product-details">
+          <i class="fa-solid fa-comment-dots ${
+            order.orders.orderStatus != 'delivered' ? 'tph-disabled' : ''
+          }" onclick="attemptProductReview(productReview, ${
+          product.product_id
+        }, ${product.order_product_id})" style="${
+          product.review_id != null
+            ? 'color: var(--tph-highlight); pointer-events: none;'
+            : ''
+        }"></i>
+            <img src="${product.product_img}" class="order-product-img" />
+            <div class="order-product-info">
+              <div class="order-product-name">${product.product_name}</div>
+              <div class="order-product-variation">${product.variation}</div>
+              <div class="order-product-quantity">x${
+                product.order_quantity
+              }</div>
+            </div>
+          </div>
+          <div>P${product.order_price}</div>
+        </div>
+        `;
+      });
+
+      let orderStatus = document.createElement('div');
+      orderStatus.classList.add('order-status-list');
+
+      orderStatus.innerHTML = `
+        <div class="order-product-status deliver">
+          ${
+            order.orders.orderStatusMsg == null
+              ? ''
+              : order.orders.orderStatusMsg
+          }
+        </div>
+        <div class="order-product-status ${
+          order.orders.orderStatus != 'cancelled'
+            ? order.orders.orderStatus != 'delivered'
+              ? 'active'
+              : 'rate'
+            : 'cancelled'
+        }">${order.orders.orderStatus}</div>
         ${
-          order.orders.orderStatusMsg == null ? '' : order.orders.orderStatusMsg
+          order.orders.orderStatus == 'processing'
+            ? `<button class="btn btn-tertiary order-product-status" onclick="attemptCancelOrder(orderCancel, ${order.orderID})"> Cancel </button>`
+            : ''
         }
-      </div>
-      <div class="order-product-status ${
-        order.orders.orderStatus != 'cancelled'
-          ? order.orders.orderStatus != 'delivered'
-            ? 'active'
-            : 'rate'
-          : 'cancelled'
-      }">${order.orders.orderStatus}</div>
-      ${
-        order.orders.orderStatus == 'processing'
-          ? `<button class="btn btn-tertiary order-product-status" onclick="attemptCancelOrder(orderCancel, ${order.orderID})"> Cancel </button>`
-          : ''
-      }
-    `;
+      `;
 
-    ordersGroup.append(productGroup, orderStatus);
-
-    container.append(ordersGroup);
-  });
+      ordersGroup.append(productGroup, orderStatus);
+      target.append(ordersGroup);
+    }
+  }
 }
 
 window.attemptCancelOrder = function attemptCancelOrder(
@@ -155,7 +171,8 @@ function attachFilterListeners() {
 }
 
 function filter(status) {
-  const container = document.querySelector('.container-orders-list');
+  const paginationPages = document.querySelector('#paginationPages');
+  const paginationController = document.querySelector('#paginationContainer');
 
   const filtered =
     status != 'all'
@@ -164,7 +181,8 @@ function filter(status) {
 
   console.log(filtered);
 
-  container.innerHTML = '';
+  paginationPages.innerHTML = '';
+  paginationController.innerHTML = '';
   appendAllPurchases(filtered);
 }
 
